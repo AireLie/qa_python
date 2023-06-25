c74ecbe1e61e69dd2f21c6d20c5a5232592d6a05
Alina <avbakhare2@mts.ru>
2023-06-25 23:35:27 +0300

    Sprint_2_first_commit

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # qa_python
+Список тест-кейсов
+1.	test_add_new_book_add_two_books - добавление 2 книг 
+2.	test_add_new_book_add_the_same_book - проверка добавления 2 одинаковых книг
+3.	test_set_book_rating_incorrect_rating[-1] - изменение рейтинга на отрицательный
+4.	test_set_book_rating_incorrect_rating[0] - изменение рейтинга на 0
+5.	test_set_book_rating_incorrect_rating[11] - изменение рейтинга на (максимальный+1)
+6.	test_set_book_rating_changing_correct_rating - корректное изменение рейтинга
+7.	test_set_book_rating_empty_book_rating - проставление рейтинга для книги не из списка
+8.	test_get_books_with_specific_rating_get_one_book - получение списка книг с определенным рейтингом
+9.	test_get_books_rating_one_book - получение словаря
+10.	test_add_book_in_favorites_not_from_list - добавление в избранное книги не из списка
+11.	test_add_book_in_favorites_correct_add - корректное добавление в избранное
+12.	test_delete_book_from_favorites_correct_deletion - удаление из избранного
+
--- tests.py
+++ tests.py
@@ -1,3 +1,4 @@
+import pytest
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -19,6 +20,79 @@ class TestBooksCollector:
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
         assert len(collector.get_books_rating()) == 2
+
+    # Добавление одной и той же книги 2 раза
+    def test_add_new_book_add_the_same_book(self):
+        collector = BooksCollector()
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        collector.add_new_book('Гордость и предубеждение и зомби')
+        assert len(collector.get_books_rating()) == 1
+
+    #параметризация
+    @pytest.mark.parametrize('rating',[-1,0,11]) # Установка некорректного рейтинга - рейтинг не изменен
+    def test_set_book_rating_incorrect_rating(self, rating):
+        collector = BooksCollector()
+        name='Гордость и предубеждение и зомби'
+        collector.add_new_book(name)
+        collector.set_book_rating(name, rating)
+        assert collector.get_book_rating(name) == 1
+
+    # Установка корректного рейтинга - рейтинг изменен
+    def test_set_book_rating_changing_correct_rating(self):
+        collector = BooksCollector()
+        first_name='Гордость и предубеждение'
+        collector.add_new_book(first_name)
+        collector.set_book_rating(first_name,8)
+        assert collector.get_book_rating(first_name) == 8
+
+    # Установка рейтинга несуществующей книге
+    def test_set_book_rating_empty_book_rating(self):
+        collector = BooksCollector()
+        collector.set_book_rating('Ежик в тумане',5)
+        assert collector.get_book_rating('Ежик в тумане') == None
+
+    # вывести книги с определенным рейтингом
+    def test_get_books_with_specific_rating_get_one_book(self):
+        collector = BooksCollector()
+        name='Золушка'
+        collector.add_new_book(name)
+        collector.set_book_rating(name,5)
+        assert collector.get_books_with_specific_rating(5)==[name]
+
+    # вывести словарь books_rating
+    def test_get_books_rating_one_book(self):
+        collector = BooksCollector()
+        name = 'Beauty and the Beast'
+        collector.add_new_book(name)
+        collector.set_book_rating(name, 9)
+        assert collector.get_books_rating() == {'Beauty and the Beast': 9}
+
+
+    # Добавить книгу в избранное не из списка
+    def test_add_book_in_favorites_not_from_list(self):
+        collector = BooksCollector()
+        collector.add_book_in_favorites('1488')
+        assert len(collector.get_list_of_favorites_books()) == 0
+
+
+    # Добавить книгу в избранное
+    def test_add_book_in_favorites_correct_add(self):
+        collector = BooksCollector()
+        collector.add_new_book('Принцесса на горошине')
+        collector.add_book_in_favorites('Принцесса на горошине')
+        assert collector.get_list_of_favorites_books() == ['Принцесса на горошине']
+
+
+    # Удаление из избранного
+    def test_delete_book_from_favorites_correct_deletion(self):
+        collector = BooksCollector()
+        tale='Rapunzel'
+        collector.add_new_book(tale)
+        collector.add_book_in_favorites(tale)
+        assert len(collector.get_list_of_favorites_books()) == 1
+        collector.delete_book_from_favorites(tale)
+        assert len(collector.get_list_of_favorites_books()) == 0
+
 
     # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
